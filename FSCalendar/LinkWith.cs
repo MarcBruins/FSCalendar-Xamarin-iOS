000ea711a63ccbc3af905c834d9254fa935ec6f9
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2017-01-17 23:46:40 -0300

    Fix indentation

--- FSCalendar/LinkWith.cs
+++ FSCalendar/LinkWith.cs
@@ -1,8 +1,3 @@
 ﻿using ObjCRuntime;
 
-[assembly: LinkWith("libFSCalendar.a",
-					LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64,
-					Frameworks = "UIKit Foundation CoreGraphics",
-					SmartLink = true,
-					ForceLoad = true,
-					LinkerFlags = "-ObjC -fobjc-arc")]
+[assembly: LinkWith("libFSCalendar.a", LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64, Frameworks = "UIKit Foundation CoreGraphics", SmartLink = true, ForceLoad = true, LinkerFlags = "-ObjC -fobjc-arc")]

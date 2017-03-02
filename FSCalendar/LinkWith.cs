44ac3ef4c391fff22cf3ccd30dfcc88d26a6585e
nmilcoff <nicolasmilcoff@hotmail.com.ar>
2017-03-02 11:47:47 -0300

    Add ObjC flag to LinkWith file

--- FSCalendar/LinkWith.cs
+++ FSCalendar/LinkWith.cs
@@ -1,3 +1,3 @@
 ﻿using ObjCRuntime;
 
-[assembly: LinkWith("libFSCalendar.a", LinkTarget.i386 | LinkTarget.x86_64 | LinkTarget.Arm64 | LinkTarget.ArmV7  | LinkTarget.Simulator, SmartLink = true, ForceLoad = true, Frameworks = "NotificationCenter UIKit Foundation CoreGraphics")] 
+[assembly: LinkWith("libFSCalendar.a", LinkTarget.i386 | LinkTarget.x86_64 | LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.Simulator, SmartLink = true, ForceLoad = true, Frameworks = "NotificationCenter UIKit Foundation CoreGraphics", LinkerFlags = "-ObjC")]
